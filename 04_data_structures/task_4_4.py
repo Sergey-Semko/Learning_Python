65ab807281de5160a1c9a16696ec1bebaca0d53d
Sergey Semko <sergey.semko@gmail.com>
2018-12-08 22:33:16 +0200

    Update task 4.4 with solution

--- 04_data_structures/task_4_4.py
+++ 04_data_structures/task_4_4.py
@@ -14,3 +14,10 @@
 
 command1 = 'switchport trunk allowed vlan 1,3,10,20,30,100'
 command2 = 'switchport trunk allowed vlan 1,3,100,200,300'
+
+#Solution
+vlans = list(set(command1.split()[-1].split(',')) & set(command2.split()[-1].split(','))) 
+vlans = [int(vlan) for vlan in vlans]
+vlans.sort()
+print(vlans)
+#[1, 3, 100]
