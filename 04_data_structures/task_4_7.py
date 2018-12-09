395ecf6e24d9e86229f013dd83a398a598c218a6
Sergey Semko <sergey.semko@gmail.com>
2018-12-09 17:51:00 +0200

    Update task 4.7 with solution

--- 04_data_structures/task_4_7.py
+++ 04_data_structures/task_4_7.py
@@ -9,3 +9,22 @@
 '''
 
 MAC = 'AAAA:BBBB:CCCC'
+
+#Solution
+MAC_list = MAC.split(":") 
+
+MAC_list 
+#['AAAA', 'BBBB', 'CCCC']
+
+#[int(x,16) for x in MAC_list] 
+#[43690, 48059, 52428]
+
+#[bin(int(x,16)) for x in MAC_list] 
+#['0b1010101010101010', '0b1011101110111011', '0b1100110011001100']
+
+MAC_list_dec = [int(x,16) for x in MAC_list] 
+MAC_list_dec 
+#[43690, 48059, 52428]
+
+print('{0[0]:08b} {0[1]:08b} {0[2]:08b}'.format(MAC_list_dec)) 
+#1010101010101010 1011101110111011 1100110011001100
