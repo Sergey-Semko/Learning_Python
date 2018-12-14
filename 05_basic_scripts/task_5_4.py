d0b96142275a92d56e2f411dd75dacacded2ffc7
Sergey Semko <sergey.semko@gmail.com>
2018-12-14 14:41:20 +0200

    Update task_5_4.py

--- 05_basic_scripts/task_5_4.py
+++ 05_basic_scripts/task_5_4.py
@@ -20,7 +20,7 @@ num_list = [10, 2, 30, 100, 10, 50, 11, 30, 15, 7]
 word_list = [
     'python', 'ruby', 'perl', 'ruby', 'perl', 'python', 'ruby', 'perl'
 ]
-
+print('num_list: ' + num_list + '\n')
 el = input('Enter the element from list: ')
 print(
     'The last index of ' + el + ' in num_list is: ' 
