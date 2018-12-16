63c7a774649c1004e6007fee4a81ccdb5f2862c0
Sergey Semko <sergey.semko@gmail.com>
2018-12-16 19:02:23 +0200

    Update task 5.1a with solution

--- 05_basic_scripts/task_5_1a.py
+++ 05_basic_scripts/task_5_1a.py
@@ -30,3 +30,24 @@ Mask:
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+
+IP_network = input("Enter IP network in format x.x.x.x/x: ")
+
+template_net = ['Network:', 
+                '{0:<10}{1:<10}{2:<10}{3:<10}', 
+                '{0:>08b}  {1:>08b}  {2:>08b}  {3:>08b}']
+template_mask1 = ['Mask:', '/{4}']
+template_mask2 = ['{:<10}{:<10}{:<10}{:<10}']
+
+net_and_mask = [int(x) for x in IP_network.replace('/', '.').split('.')]
+net_and_mask = [net_and_mask[4-i] for i in range((32 - net_and_mask[-1])/8)]
+mask_bin = [('1' * net_and_mask[-1] + '0' * (32 - net_and_mask[-1]))
+           [0 + i:8 + i] for i in range(0, 25, 8)]
+mask_dec = [int(x, 2) for x in mask_bin]
+
+print ('\n')
+print ('\n'.join(template_net).format(*net_and_mask))
+print ('\n'.join(template_mask1).format(*net_and_mask))
+print ('\n')
+print (template_mask2.format(*mask_dec))
+print (template_mask2.format(*mask_bin))
