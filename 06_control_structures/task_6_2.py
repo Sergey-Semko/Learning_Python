77360586f373c5df0b6846fd996047c86d01ae4e
Sergey Semko <sergey.semko@gmail.com>
2018-12-25 20:31:37 +0200

    Update task 6.2 with solution

--- 06_control_structures/task_6_2.py
+++ 06_control_structures/task_6_2.py
@@ -14,3 +14,8 @@
 mac = ['aabb:cc80:7000', 'aabb:dd80:7340', 'aabb:ee80:7000', 'aabb:ff80:7000']
 
 mac_cisco = []
+
+for addr in mac:
+    mac_cisco.append(addr.replace(':', '.'))
+print(mac)
+print(mac_cisco)
