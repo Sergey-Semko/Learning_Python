129ab4fbcc24dcfd9bdc377a38abc4f4d64d16b8
Sergey Semko <sergey.semko@gmail.com>
2019-01-07 21:06:43 +0200

    Update task 6_3 with solution, first iteration

--- 06_control_structures/task_6_3.py
+++ 06_control_structures/task_6_3.py
@@ -1,25 +1,19 @@
 # -*- coding: utf-8 -*-
 '''
 Задание 6.3
-
 В скрипте сделан генератор конфигурации для access-портов.
-
 Сделать аналогичный генератор конфигурации для портов trunk.
-
 В транках ситуация усложняется тем, что VLANов может быть много, и надо понимать,
 что с ним делать.
-
 Поэтому в соответствии каждому порту стоит список
 и первый (нулевой) элемент списка указывает как воспринимать номера VLAN,
 которые идут дальше:
 	add - значит VLANы надо будет добавить (команда switchport trunk allowed vlan add 10,20)
 	del - значит VLANы надо удалить из списка разрешенных (команда switchport trunk allowed vlan remove 17)
 	only - значит, что на интерфейсе должны остаться разрешенными только указанные VLANы (команда switchport trunk allowed vlan 11,30)
-
 Задача для портов 0/1, 0/2, 0/4:
 - сгенерировать конфигурацию на основе шаблона trunk_template
 - с учетом ключевых слов add, del, only
-
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
 
@@ -46,11 +40,20 @@ fast_int = {
         '0/4': ['del', '17']
     }
 }
-
+'''
 for intf, vlan in fast_int['access'].items():
     print('interface FastEthernet' + intf)
     for command in access_template:
         if command.endswith('access vlan'):
             print(' {} {}'.format(command, vlan))
+        else:
+            print(' {}'.format(command))
+'''
+for intf, vlan in fast_int['trunk'].items():
+    print('interface FastEthernet ' + intf)
+    for command in trunk_template:
+        if command.endswith('allowed vlan'):
+            if vlan[0] == "add":
+            print(' {} {}'.format(command, vlan[1]))
         else:
             print(' {}'.format(command))
