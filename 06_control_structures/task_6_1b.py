322d9a39fb38272d1f53adc144b2a4beb00df3a8
Sergey Semko <sergey.semko@gmail.com>
2019-01-10 16:34:25 +0200

    Update task 6.1b with solution

--- 06_control_structures/task_6_1b.py
+++ 06_control_structures/task_6_1b.py
@@ -9,3 +9,42 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+#Solution
+address_correct = False
+while not address_correct:
+	IP_address = input("Enter IP address in format 10.0.1.1: ")
+    if IP_address.count('.') == 3:
+        IP_address = [int(x) for x in IP_address.split('.')]
+        for part in IP_address:
+            if part in range(0,256):
+                address_correct = True
+            else:
+                address_correct = False
+                break
+    else:
+        print('Incorrect IPv4 address')
+
+if (
+  IP_address[0] in range(1, 128)
+  or IP_address[0] in range(127, 192)
+  or IP_address[0] in range (191, 224)
+):
+    print('unicast')
+elif IP_address[0] in range(223, 240):
+    print('multicast')
+elif (
+  IP_address[0]==255
+  and IP_address[1]==255
+  and IP_address[2]==255
+  and IP_address[3]==255
+):
+    print('local broadcast')
+elif (
+  IP_address[0]==0
+  and IP_address[1]==0
+  and IP_address[2]==0
+  and IP_address[3]==0
+):
+    print('unassigned')
+else:
+    print('unused')
