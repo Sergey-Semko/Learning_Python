9faa905dd1056aa16544bf7a45dd63875c63f116
Sergey Semko <sergey.semko@gmail.com>
2019-01-10 15:35:54 +0200

    Update task 6.1a with solution, refactoring

--- 06_control_structures/task_6_1a.py
+++ 06_control_structures/task_6_1a.py
@@ -16,7 +16,8 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
-#Solution
+#Solution 1
+'''
 try:
     IP_address = [int(x) for x in input("Enter IP address in format 10.0.1.1: ").split('.')]
     for i in range(4):
@@ -50,3 +51,40 @@ else:
         print('unassigned')
     else:
         print('unused')
+'''
+#Solution 2
+IP_address = input("Enter IP address in format 10.0.1.1: ")		
+if IP_addrss.count('.') == 3:
+    IP_address = [int(x) for x in IP_address.split('.')]
+    for part in IP_address:
+        if part not in range(0,256)
+            print('Incorrect IPv4 address')
+            exit()
+else:
+    print('Incorrect IPv4 address')
+    exit()
+
+if (
+  IP_address[0] in range(1, 128)
+  or IP_address[0] in range(127, 192)
+  or IP_address[0] in range (191, 224)
+):
+    print('unicast')
+elif IP_address[0] in range(223, 240):
+    print('multicast')
+elif (
+  IP_address[0]==255
+  and IP_address[1]==255
+  and IP_address[2]==255
+  and IP_address[3]==255
+):
+    print('local broadcast')
+elif (
+  IP_address[0]==0
+  and IP_address[1]==0
+  and IP_address[2]==0
+  and IP_address[3]==0
+):
+    print('unassigned')
+else:
+    print('unused')
