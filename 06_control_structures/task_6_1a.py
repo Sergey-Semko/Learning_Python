45b96956cbfd17a5e8d039174b90f76bd93dbb49
Sergey Semko <sergey.semko@gmail.com>
2019-01-10 14:48:33 +0200

    Update task 6.1a with solution, Indentation fix

--- 06_control_structures/task_6_1a.py
+++ 06_control_structures/task_6_1a.py
@@ -16,35 +16,36 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+#Solution
 try:
     IP_address = [int(x) for x in input("Enter IP address in format 10.0.1.1: ").split('.')]
     for i in range(4):
         if IP_address not in range(0, 256):
             print('Incorrect IPv4 address')
 except (ValueError, IndexError):
-   print('Incorrect IPv4 address')
+    print('Incorrect IPv4 address')
 else:
-   if (
+    if (
       IP_address[0] in range(1, 128)
       or IP_address[0] in range(127, 192)
       or IP_address[0] in range (191, 224)
-   ):
-      print('unicast')
-   elif IP_address[0] in range(223, 240):
-      print('multicast')
-   elif (
+    ):
+        print('unicast')
+    elif IP_address[0] in range(223, 240):
+        print('multicast')
+    elif (
       IP_address[0]==255
       and IP_address[1]==255
       and IP_address[2]==255
       and IP_address[3]==255
-   ):
-      print('local broadcast')
-  elif (
+    ):
+        print('local broadcast')
+    elif (
       IP_address[0]==0
       and IP_address[1]==0
       and IP_address[2]==0
       and IP_address[3]==0
-  ):
-      print('unassigned')
-  else:
-      print('unused')
+    ):
+        print('unassigned')
+    else:
+        print('unused')
