f84f1c02bbb5abc0b565fbbe10043f581ebd9320
Sergey Semko <sergey.semko@gmail.com>
2019-01-18 23:25:50 +0200

    Update task 7_1 with solution

--- 07_files/task_7_1.py
+++ 07_files/task_7_1.py
@@ -14,3 +14,11 @@ Outbound Interface:    FastEthernet0/0
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+file.open('ospf.txt', 'r')
+
+route_template = "Protocol:              OSPF\nPrefix:                {1}\nAD/Metric:             {2}\nNext-Hop:              {4}\nLast update:           {5}\nOutbound Interface:    {6}"
+
+for line in file:
+    print(route_template.format(*line.replace('[', '').replace(']', '').replace(',', '').split()))
+
+file.close()
