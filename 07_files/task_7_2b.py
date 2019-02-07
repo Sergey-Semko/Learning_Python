d3afe39c8d5a7121d928ca2dac95362f7ef97463
Sergey Semko <sergey.semko@gmail.com>
2019-02-07 22:04:17 +0200

    Update task 7.2b with solution

--- 07_files/task_7_2b.py
+++ 07_files/task_7_2b.py
@@ -13,4 +13,23 @@
 
 '''
 
+from sys import argv
+
 ignore = ['duplex', 'alias', 'Current configuration']
+ignore_presence = False
+
+file_name = argv[1]
+file_in = open(file_name, 'r')
+file_out = open('config_sw1_cleared.txt', 'w')
+
+for line in file_in:
+    for word in ignore:
+        if line.find(word) > -1:
+            ignore_presence = True
+            break
+    if not ignore_presence:
+        file_out.write(line)
+    else:
+        ignore_presence = False
+file_in.close()
+file_out.close()
