da3466ab1c4c57f5196999f0d93293bdb95ea889
Sergey Semko <sergey.semko@gmail.com>
2019-02-10 18:53:13 +0200

    Update task 7_3b with solution

--- 07_files/task_7_3b.py
+++ 07_files/task_7_3b.py
@@ -11,3 +11,15 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+file = open('CAM_table.txt', 'r')
+
+vlans_list = []
+for line in file:
+    if line.count('.') == 2:
+        vlans_list.append(line.strip())
+vlans_list.sort()
+vlan = input ('Номер VLAN-a по которому необходима информация: ')
+for item in vlans_list:
+    if item.find(vlan) == 0
+        print(' {0:8}{1:17}{3}'.format(*item.split()))
+file.close()
