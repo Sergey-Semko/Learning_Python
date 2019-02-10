f0ca896ee3387d9460fb068927e4af4829fae3f6
Sergey Semko <sergey.semko@gmail.com>
2019-02-10 18:32:32 +0200

    Update task 7.3a with solution

--- 07_files/task_7_3a.py
+++ 07_files/task_7_3a.py
@@ -11,3 +11,13 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+file = open('CAM_table.txt', 'r')
+
+vlans_list = []
+for line in file:
+    if line.count('.') == 2:
+        vlans_list.append(line.strip())
+vlans_list.sort()
+for item in vlans_list:
+    print(' {0:8}{1:17}{3}'.format(*item.split()))
+file.close()
