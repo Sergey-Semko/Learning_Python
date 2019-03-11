fb141679c47af19ca7e3f658451a787bd8fff0af
Sergey Semko <sergey.semko@gmail.com>
2019-03-11 22:22:05 +0200

    Update task 9.1b with solution

--- 09_functions/task_9_1b.py
+++ 09_functions/task_9_1b.py
@@ -22,18 +22,16 @@
 '''
 
 
-def generate_access_config(access):
+def generate_access_config(access, psecurity=False):
     '''
     access - словарь access-портов,
     для которых необходимо сгенерировать конфигурацию, вида:
         { 'FastEthernet0/12':10,
           'FastEthernet0/14':11,
           'FastEthernet0/16':17 }
-
     psecurity - контролирует нужна ли настройка Port Security. По умолчанию значение False
         - если значение True, то настройка выполняется с добавлением шаблона port_security
         - если значение False, то настройка не выполняется
-
     Функция возвращает словарь:
     - ключи: имена интерфейсов, вида 'FastEthernet0/1'
     - значения: список команд, который надо выполнить на этом интерфейсе
@@ -51,6 +49,19 @@ def generate_access_config(access):
         'switchport port-security'
     ]
 
+    list_of_conf = {}
+    for intf, vlan in access.items():
+        list_of_conf[intf] = []
+        for command in access_template:
+            if command.endswith('access vlan'):
+                list_of_conf[intf].append('{} {}'.format(command, vlan))
+            else:
+                list_of_conf[intf].append(command)
+        if psecurity:
+            for command in port_security:
+                list_of_conf[intf].append(command)
+    return list_of_conf
+
 
 access_dict = {
     'FastEthernet0/12': 10,
@@ -58,3 +69,7 @@ access_dict = {
     'FastEthernet0/16': 17,
     'FastEthernet0/17': 150
 }
+
+print(generate_access_config(access_dict))
+print('#' * 40)
+print(generate_access_config(access_dict, True))
