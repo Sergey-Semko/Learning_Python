79e1d6e0d24876eebdc3379d823ce0050c5a0e3a
Sergey Semko <sergey.semko@gmail.com>
2019-03-14 21:11:24 +0200

    Update task 9.3a with solution

--- 09_functions/task_9_3a.py
+++ 09_functions/task_9_3a.py
@@ -23,3 +23,24 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+def get_int_vlan_map(conf_file):
+    access_dict = {}
+    trunk_dict = {}
+    with open(conf_file) as f:
+        for line in f:
+            if 'interface' in line:
+                interface = line.split()[-1]
+            elif 'mode access' in line:
+                access_dict[interface] = 1
+            elif 'access vlan' in line:
+                vlan = int(line.split()[-1])
+                access_dict[interface] = vlan
+            elif 'trunk allowed vlan' in line:
+                vlans = [int(item) for item in line.split()[-1].split(',')]
+                trunk_dict[interface] = vlans
+    return access_dict, trunk_dict
+
+access_d, trunk_d = get_int_vlan_map('config_sw2.txt')
+
+print(access_d)
+print(trunk_d)
