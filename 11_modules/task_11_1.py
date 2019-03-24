8ced844725a5051ad86c4f0252b18f94206245be
Sergey Semko <sergey.semko@gmail.com>
2019-03-24 23:15:10 +0200

    Update task 11.1 with solution

--- 11_modules/task_11_1.py
+++ 11_modules/task_11_1.py
@@ -27,3 +27,23 @@ R6           Fa 0/2          143           R S I           2811       Fa 0/0
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+def parse_cdp_neighbors (input):
+    commands = input.split('\n')
+    dict = {}
+    for command in commands:
+        if command.startswith(' ') or command == '':
+            pass
+        elif 'show cdp neighbors' in command:
+            l_name = command.split('>')[0]
+        else:
+            r_name, l_intf, l_port, h_time, *other, r_intf, r_port = command.split()
+            if h_time[0].isdigit():
+                dict[(l_name, l_intf + l_port)] = (r_name, r_intf + r_port)
+    return dict
+
+if __name__ == "__main__":
+    f = open('sw1_sh_cdp_neighbors.txt')
+    parse_com = parse_cdp_neighbors(f.read())
+    f.close()
+
+    print(parse_com)
